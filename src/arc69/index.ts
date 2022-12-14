8ee1b22503bc191461cd7361e580522d44c2c449
ElianArismendi <elian.arismendi@interatica.com>
2022-12-14 11:27:29 -0300

    create getDigitalMedia method for arc3

--- src/arc69/index.ts
+++ src/arc69/index.ts
@@ -1,28 +1,57 @@
 import { Indexer } from "algosdk";
 import { ArcEnum } from "../enum/arc.enum";
+import { Errors } from "../enum/errors.enum";
 import { AssetInfo } from "../types/asset-info.interface";
 import { Arc69Metadata } from "./types/json.scheme";
 
 export abstract class Arc69 {
-    static async checkIfValidArc(asaInfo: AssetInfo, indexer: Indexer) {
-        const metadata = await Arc69.getLastCfgTxnNoteParsed(asaInfo.index, indexer);
-        if(metadata.standard === ArcEnum.arc69) {
-            return true;
-        } else {
-            return false;
-        }
+  static async checkIfValidArc(asaInfo: AssetInfo, indexer: Indexer) {
+    const metadata = await Arc69.getLastCfgTxnNoteParsed(
+      asaInfo.index,
+      indexer
+    );
+    return metadata.standard === ArcEnum.arc69;
+  }
+  static async getLastCfgTxnNoteParsed(
+    asaId: number,
+    indexer: Indexer
+  ) {
+    const response = (await indexer
+      .lookupAssetTransactions(asaId)
+      .txType("acfg")
+      .do()) as any[];
+    const lastTxn = response["transactions"].pop();
+    return JSON.parse(Buffer.from(lastTxn.note, "base64").toString());
+  }
+  static async getMetadata(
+    info: AssetInfo,
+    indexer: Indexer
+  ): Promise<Arc69Metadata> {
+    try {
+      const metadata = await this.getLastCfgTxnNoteParsed(
+        info.index,
+        indexer
+      );
+      return metadata;
+    } catch (error) {
+      throw new Error(`Arc 69 ${Errors.arcBadConfigured} ` + error);
     }
-    static async getLastCfgTxnNoteParsed(asaId: number, indexer: Indexer) {
-        const response = await indexer.lookupAssetTransactions(asaId).txType("acfg").do() as any[];
-        const lastTxn = response["transactions"].pop()
-        return JSON.parse(Buffer.from(lastTxn.note, "base64").toString());
+  }
+  static async getDigitalMedia(
+    info: AssetInfo,
+    indexer: Indexer
+  ): Promise<string[]> {
+    try {
+      const { external_url, media_url, ...asset } =
+        await this.getMetadata(info, indexer);
+      const media = [];
+      info.params.url?.startsWith("ipfs://") &&
+        media.push(info.params.url.split("/").pop());
+      media_url && media.push(media_url);
+      external_url && media.push(external_url);
+      return media;
+    } catch (error) {
+      throw new Error(`Arc 69 ${Errors.arcBadConfigured} ` + error);
     }
-    static async getMetadata(info: AssetInfo, indexer: Indexer): Promise<Arc69Metadata> {
-        try {
-            const metadata = await this.getLastCfgTxnNoteParsed(info.index, indexer);
-            return metadata;
-        } catch (error) {
-            throw new Error("Arc 19 not configured correctly. Asset url is invalid. " + error);
-        }
-    }
+  }
 }
